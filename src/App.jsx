5203cdd401200b36ed3ab63fc78a91bbd6995908
Jacob Tham <jacobtham3@gmail.com>
2025-05-27 21:40:46 +0800

    slight code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,55 @@
 // src/App.jsx
-
 import "./App.css";
 import IngredientList from "./components/IngredientList/IngredientList";
 import BurgerStack from "./components/BurgerStack/BurgerStack";
 import { useState } from "react";
+const availableIngredients = [
+  { name: "Kaiser Bun", color: "saddlebrown" },
+  { name: "Sesame Bun", color: "sandybrown" },
+  { name: "Gluten Free Bun", color: "peru" },
+  { name: "Lettuce Wrap", color: "olivedrab" },
+  { name: "Beef Patty", color: "#3F250B" },
+  { name: "Soy Patty", color: "#3F250B" },
+  { name: "Black Bean Patty", color: "#3F250B" },
+  { name: "Chicken Patty", color: "burlywood" },
+  { name: "Lettuce", color: "lawngreen" },
+  { name: "Tomato", color: "tomato" },
+  { name: "Bacon", color: "maroon" },
+  { name: "Onion", color: "lightyellow" },
+  { name: "Cheddar Cheese", color: "#FDE18B" },
+  { name: "Swiss Cheese", color: "#F1E1A8" },
+];
 
 const App = () => {
-  const availableIngredients = [
-    { name: "Kaiser Bun", color: "saddlebrown" },
-    { name: "Sesame Bun", color: "sandybrown" },
-    { name: "Gluten Free Bun", color: "peru" },
-    { name: "Lettuce Wrap", color: "olivedrab" },
-    { name: "Beef Patty", color: "#3F250B" },
-    { name: "Soy Patty", color: "#3F250B" },
-    { name: "Black Bean Patty", color: "#3F250B" },
-    { name: "Chicken Patty", color: "burlywood" },
-    { name: "Lettuce", color: "lawngreen" },
-    { name: "Tomato", color: "tomato" },
-    { name: "Bacon", color: "maroon" },
-    { name: "Onion", color: "lightyellow" },
-    { name: "Cheddar Cheese", color: "#FDE18B" },
-    { name: "Swiss Cheese", color: "#F1E1A8" },
-  ];
-
   const [ingredient, setIngredient] = useState(availableIngredients);
   const [stack, setStack] = useState([]);
 
   const addIngredient = (event) => {
     let addedBurger = { name: "", color: "" };
-    addedBurger.name = event.target.name;
-    addedBurger.color = event.target.parentElement.style.backgroundColor;
+    Object.assign(
+      addedBurger,
+      { name: event.target.name },
+      { color: event.target.parentElement.style.backgroundColor }
+    );
 
-    let newstack = [];
-    newstack = [...stack, addedBurger];
-    setStack(newstack);
-
-    let newList = [
+    setStack([...stack, addedBurger]);
+    setIngredient([
       ...ingredient.filter((ingredient) => ingredient.name != addedBurger.name),
-    ];
-    setIngredient(newList);
-    console.log(ingredient);
+    ]);
   };
 
   const removeIngredient = (event) => {
     let removedBurger = { name: "", color: "" };
-    removedBurger.name = event.target.name;
-    removedBurger.color = event.target.parentElement.style.backgroundColor;
-    let newstack = [
-      ...stack.filter((ingredient) => ingredient.name != removedBurger.name),
-    ];
-    setStack(newstack);
+    Object.assign(
+      removedBurger,
+      { name: event.target.name },
+      { color: event.target.parentElement.style.backgroundColor }
+    );
 
-    let newList = [...ingredient, removedBurger];
-    setIngredient(newList);
+    setStack([
+      ...stack.filter((ingredient) => ingredient.name != removedBurger.name),
+    ]);
+    setIngredient([...ingredient, removedBurger]);
   };
 
   return (
